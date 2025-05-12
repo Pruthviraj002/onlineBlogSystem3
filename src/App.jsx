3fcfa75b769f2be299b6ee0a4a0e2e9926c9c97a
gitDemo <sy9535393@gmail.com>
2025-05-12 23:04:55 +0530

    create first tempale page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Dashboard from './Pages/Dashboard'
+import Signin from './Pages/Signin'
+import Signup from './Pages/Signup'
+import Project from './Pages/Project'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-7xl text-red-800'>hello world</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/sign-in' element={<Signin />} />
+          <Route path='/sign-up' element={<Signup />} />
+          <Route path='/project' element={<Project />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
